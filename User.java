5be57d6d0c9db63d8c2c10d7b39ae254f0d79894
Babor Ali <ali2305101954@diu.edu.bd>
2024-12-03 00:32:34 +0600

    Add files via upload
    Here ,This is my 1st commit in this project.

---
+++ User.java
@@ -0,0 +1,40 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    // Constructor to initialize user data
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    // Getters for the user properties
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    // Setters for updating user properties (if needed)
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
